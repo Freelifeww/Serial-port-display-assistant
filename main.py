12724433fd62080b19b260d9bb5b9277fcfcec2a
wangwei@tuya.com <wangwei@tuya.com>
2021-06-12 20:49:54 +0800

    add loop serial

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
 import serial
 import serial.tools.list_ports
-import serial_gui               #user file
+import time
+import threading
 
-plist = list(serial.tools.list_ports.comports())  #get serial list
-com_num = len(plist)                              #serial list number
+import serial_gui                #user file
 
-lable_x = 0
-lable_y = 0
+def serial_loop():
+    while True:
+        lable_x = 0
+        lable_y = 0
+        plist = list(serial.tools.list_ports.comports())  #get serial list
+        com_num = len(plist)                              #serial list number
+        if com_num <= 0:
+            print ("The Serial port can't find!")
+        else:
+            for index in range(com_num):
+                plist_com =list(plist[index])
+                serial_name = plist_com[0]
+                serial_gui.gui_lable_dispaly(serial_name,lable_x,lable_y)
+                lable_y=lable_y+0.1
+    time.sleep(1500)
 
-if com_num <= 0:
-    print ("The Serial port can't find!")
-else:
-    print ("system all com number is",com_num)
-    for index in range(com_num):
-        plist_com =list(plist[index])
-        serial_name = plist_com[0]
-        serial_gui.gui_lable_dispaly(serial_name,lable_x,lable_y)
-        lable_y=lable_y+0.1
-        print ("com name is:",serial_name)
+serial_gui.gui_init()
 
-serial_gui.gui_loop()
+serial_thread = threading.Thread(target=serial_loop)
+serial_thread.start()
 
-
+gui_thread = threading.Thread(target=serial_gui.gui_loop())
+gui_thread.start()
--- serial_gui.py
+++ serial_gui.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 import tkinter
 
+
+
 root = tkinter.Tk()
 
+def gui_init():
+    root.title('serial com')
+    root.geometry('100x100')
+
 def gui_lable_dispaly(name,x,y):
-        root.title('serial com')
-        root.geometry('200x200')
-        serial_label = tkinter.Label(root,text=name)
+        serial_label = tkinter.Label(root,text=name,bg='green')
         serial_label.place(relx=x,rely=y)
 
 def gui_loop():
